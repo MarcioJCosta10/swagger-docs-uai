625571cfe3535661570e3299c44a8ec45a5dbafe
MarcioJCosta10 <marciojcostadev@gmail.com>
2026-01-12 12:32:41 -0300

    Swagger Uai - estrutura inicial

---
+++ docs/swagger-initializer.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  SwaggerUIBundle({
+    url: "../swagger-docs-uai.yml",
+    dom_id: "#swagger-ui",
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout"
+  });
+};
